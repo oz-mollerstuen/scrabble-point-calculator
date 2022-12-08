bf23da033706fca0594ff042429659893282698a
robbryandev <robbryandev@gmail.com>
2022-12-08 10:40:02 -0800

    Add initial calculator
    co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>

---
+++ ScrabbleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ScrabbleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Enter A Word For Me To Score:");
+            string? wordInp = Console.ReadLine();
+            string[] letters = File.ReadAllLines("./data/input.txt");
+            Console.WriteLine("The words score is " + Scrabble.GetScore(wordInp, letters));
+            Main();
+        }
+    }
+}
